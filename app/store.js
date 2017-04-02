3a9a64876f881737d601d9fc51915d4d52341fa6
Prudhvi Dharmavarapu <akira.prudhvi@gmail.com>
2017-04-02 22:30:13 +0530

    logger disable in production

--- app/store.js
+++ app/store.js
@@ -9,7 +9,7 @@ import createSagaMiddleware from 'redux-saga';
 import createReducer from './reducers';
 import createLogger from 'redux-logger';
 
-const logger = createLogger({
+const logger = process.env.NODE_ENV !== 'production' && createLogger({
   // Ignore `CHANGE_FORM` actions in the logger, since they fire after every keystroke
   predicate: (getState, action) => action.type !== 'CHANGE_FORM',
 });
