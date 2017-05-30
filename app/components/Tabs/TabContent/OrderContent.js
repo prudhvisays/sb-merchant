09068aa004da6d86e8e9f14793221c67f2519fd4
Prudhvi Dharmavarapu <akira.prudhvi@gmail.com>
2017-05-30 21:40:28 +0530

    cash collection status

--- app/components/Tabs/TabContent/OrderContent.js
+++ app/components/Tabs/TabContent/OrderContent.js
@@ -12,15 +12,15 @@ export default class OrderContent extends React.Component { //eslint-disable-lin
             <OrderContentStyle className="row">
                 <div className="col-12">
                     <div className="d-flex flex-row">
-                        <div className="col-md-5 hidden-md-down d-flex flex-column">
+                        <div className="col-md-4 hidden-md-down d-flex flex-column">
                             <div className="content-subtitle">Title</div>
                             <div className="content-title">{data.title}</div>
                         </div>
-                        <div className="col-md-5 hidden-lg-up d-flex flex-column">
+                        <div className="col-md-4 hidden-lg-up d-flex flex-column">
                           <div className="content-subtitle">Cost</div>
                           <div className="content-title">{data.final_cost}</div>
                         </div>
-                        <div className="col-md-5 d-flex flex-column">
+                        <div className="col-md-4 d-flex flex-column">
                             <div className="content-subtitle">Final Kms</div>
                             <div className="content-title">{data.distance_picked_to_delivery_in_meters}</div>
                         </div>
@@ -28,6 +28,10 @@ export default class OrderContent extends React.Component { //eslint-disable-lin
                             <div className="content-subtitle">Payment Type</div>
                             <div className="content-title">{data.paymentType}</div>
                         </div>
+                      { data.status.toLowerCase() === 'completed' && <div className="col-md-2 d-flex flex-column">
+                          <div className="content-subtitle">Cash Collected</div>
+                          <div className="content-title">{data.cash_collected ? 'Yes' : 'No'}</div>
+                        </div>  }
                     </div>
                     <hr/>
                     <div className="d-flex flex-row">
